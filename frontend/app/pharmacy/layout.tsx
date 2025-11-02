e324e251384f89deb1111aa4acc7d8ae466ebdfb
Hashinie-Madhushika <madhushikahashinie45@gmail.com>
2025-11-02 22:41:40 +0530

    Left Side Navigation bar for a pharmacy part

---
+++ frontend/app/pharmacy/layout.tsx
@@ -0,0 +1,19 @@
+// app/pharmacy/layout.tsx
+"use client";
+import React from "react";
+import SidebarPharmacy from "../../components/SidbarPharmacy";
+
+export default function PharmacyLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen bg-gray-50">
+      <SidebarPharmacy />
+      <main className="flex-1 ml-64 p-6">
+        {children}
+      </main>
+    </div>
+  );
+}
